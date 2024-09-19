b1010bb5195d1ce229927c6e9ad98124dc1f4e25
luticakep <kaylasorayadj@gmail.com>
2024-09-19 17:51:10 +0700

    complete assignment 4

--- vinyl_shop/settings.py
+++ vinyl_shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n3#$d5g6q7z5m*bvpb)@k*$sm6p#xv6(b(^=aymg5om))$6ame'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kayla-soraya-vinylshop.pbp.cs.ui.ac.id"]
 
